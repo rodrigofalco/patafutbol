1f9ab1c7200540e49bb2a173174960734dec6612
Rodrigo Ignacio Falco <rfalco@patagoniansys.com>
2014-11-21 13:17:40 -0300

    Some aesthetics changes.

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -1,8 +1,12 @@
 'use strict';
 var mongoose = require('mongoose');
 var Player = mongoose.model('Player', { name: String, nick: String });
+
 /**
- * Module dependencies.
+ * Index html.
+ * This is the main container for the angular app.
+ * There should be no passing database objects to the view,
+ * as this are latter interchanged between angular and express.
  */
 exports.index = function(req, res) {
 	res.render('index', {
@@ -11,14 +15,16 @@ exports.index = function(req, res) {
 	});
 };
 
+/**
+* Temporal controller that returns players json.
+*/
 exports.players = function(req, res) {
-	
 	Player.find(function (err, players) {
 	  if (err) { 
 	  	console.error(err);
 	  	res.send({ errors: err });
 	  }
-	  console.log(players);
+	  //console.log(players);
 	  res.send({ players: players });
 	});
 };
