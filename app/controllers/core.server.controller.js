9c3f0f054ea252d8aec8d3e452e069c8b796da57
Rodrigo Ignacio Falco <rfalco@patagoniansys.com>
2014-11-20 22:46:47 -0300

    First request

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -1,5 +1,6 @@
 'use strict';
-
+var mongoose = require('mongoose');
+var Player = mongoose.model('Player', { name: String, nick: String });
 /**
  * Module dependencies.
  */
@@ -7,5 +8,17 @@ exports.index = function(req, res) {
 	res.render('index', {
 		user: req.user || null,
 		request: req
+	});
+};
+
+exports.players = function(req, res) {
+	
+	Player.find(function (err, players) {
+	  if (err) { 
+	  	console.error(err);
+	  	res.send({ errors: err });
+	  }
+	  console.log(players);
+	  res.send({ players: players });
 	});
 };
--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,13 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$http',
+	function($scope, Authentication, $http) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+		$http.get('players').success(function(data) {
+		  $scope.players = data.players;
+		});
 	}
 ]);
